46959e3a0307e7f672bbde7f6f2bc81df26cc806
Om Bambale <bambale.om@gmail.com>
2025-07-09 10:33:10 +0530

    day9

---
+++ Day 8/src/com/tnsif/dayeight/nesteddemo/Execution.java
@@ -0,0 +1,10 @@
+package com.tnsif.dayeight.nesteddemo;
+
+public class Execution {
+	public sttatic void main(String[] args) {
+		NestedDemo n=new NestedDemo();
+		n.calArea();
+		n.print();
+	}
+
+}
